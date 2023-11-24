ee349a7fb8411e6886e246b783582f90f0a661af
Juan Jose huertas <jjhuertasbotache@gmail.com>
2023-11-24 09:46:30 -0500

    small fix in useAbove

--- 7)SWapiProject/src/Hooks/useAbove.jsx
+++ 7)SWapiProject/src/Hooks/useAbove.jsx
@@ -4,12 +4,14 @@ import { useState } from "react";
 function defaultHandlerFunction() {
     return (document.documentElement.scrollTop > 15).toString();
 }
+
+
 export default function useAbove(handlerFunction=defaultHandlerFunction) {
 
     const [isAbove, setIsAbove] = useState(handlerFunction.toString() || "false");
 
     function checkAbove() {
-        setIsAbove((document.documentElement.scrollTop > 15).toString());        
+        setIsAbove(handlerFunction.toString() || "false");        
     }
 
     return { isAbove, checkAbove };
